03fabfea18de1a27d7b5a404765b022ebe121907
Luis Climaco <luis.cordoba.mt@usa.edu.co>
2022-10-10 22:44:33 -0500

    Se agregan mas tablas

---
+++ src/main/java/com/usa/misiontic23/masterclass3/entities/Reservas.java
@@ -0,0 +1,73 @@
+package com.usa.misiontic23.masterclass3.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "reservas")
+
+public class Reservas implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String box;
+    private String client;
+    private Date fechainicio;
+    private Date fechafin;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    @JsonIgnoreProperties("reservas")
+    private Category category;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBox() {
+        return box;
+    }
+
+    public void setBox(String box) {
+        this.box = box;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public Date getFechainicio() {
+        return fechainicio;
+    }
+
+    public void setFechainicio(Date fechainicio) {
+        this.fechainicio = fechainicio;
+    }
+
+    public Date getFechafin() {
+        return fechafin;
+    }
+
+    public void setFechafin(Date fechafin) {
+        this.fechafin = fechafin;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
